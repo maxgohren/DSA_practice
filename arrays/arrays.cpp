db4498cd120cfb3bb14dcc60b5636f57c4cdf1eb
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 <maxgohren@gmail.com>
2024-01-19 12:54:08 -0500

    array adt functions

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -1,5 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+struct Array {
+    int A[10];
+    int size;
+    int length;
+};
+
+int Get(struct Array arr, int index)
+{
+    if(index>=0 && index<arr.length)
+            return arr.A[index];
+    return -1;
+}
+
+void Set(struct Array *arr, int index, int x)
+{
+    if(index>=0 && index<arr->length)
+        arr->A[index] = x;
+}
+
+int Max(struct Array arr)
+{
+  int max=arr.A[0];
+  int i;
+  for(i=1;i<arr.length;i++)
+  {
+    if(arr.A[i]>max)
+      max=arr.A[i];
+  }
+  return max;
+}
+
+int Min(struct Array arr)
+{
+  int min=arr.A[0];
+  int i;
+  for(i=1;i<arr.length;i++)
+  {
+    if(arr.A[i]<min)
+      min=arr.A[i];
+  }
+  return min;
+}
+
+int Sum(struct Array arr)
+{
+  int s=0;
+  int i;
+  for(i=0;i<arr.length;i++)
+      s+=arr.A[i];
+  return s;
+}
+
+float Avg(struct Array arr)
+{
+  return (float)Sum(arr)/arr.length;
+}
+
+void Display(struct Array arr)
+{
+    for(int i = 0; i<arr.length;i++)
+    {
+        printf("%d\n", arr.A[i]);
+    }
+}
 
 int main()
 {
@@ -55,7 +123,18 @@ int main()
 
     /* 2D Arrays */
 
-   
+    /* Array ADT Methods */
+    struct Array arr = {{2,3,4,5,6}, 10,5};
+    
+
+    Set(&arr, 0, 15);
+    Display(arr);
+    cout << "Max:" << Max(arr) << endl;
+    cout << "Min:" << Min(arr) << endl;
+    cout << "Avg:" << Avg(arr) << endl;
+
+    
+       
 
 
     return 0;
