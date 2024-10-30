1404919cb370213714d056de0ab39b120b9619ed
Max Gohren <maxgohren@gmail.com>
2024-10-30 16:20:08 -0400

    BUG: deleting last in list needs fixin'

--- linkedlist/circlist.c
+++ linkedlist/circlist.c
@@ -6,8 +6,21 @@ struct Node{
 	struct Node *next;
 }*Head;
 
-void create(int A[], int n)
+void Display(struct Node *h)
 {
+	do{
+		if( h->next != Head)
+			printf("%d -> ", h->data);
+		else
+			printf("%d ", h->data);
+		h=h->next;
+	}while(h!=Head);
+	printf("\n");
+}
+
+void createCirc(int A[], int n)
+{
+	printf("Creating list:\n");
 	int i;
 	struct Node *t, *last;
 
@@ -24,22 +37,127 @@ void create(int A[], int n)
 		last->next = t;
 		last = t;
 	}
+	Display(Head);
 }
 
-void Display(struct Node *h)
+
+void Append(int n)
 {
+	printf("Appending node %d\n", n);
+	struct Node *last = Head;
+
+	// Move last to "end" of list
 	do{
-		printf("%d ", h->data);
-		h=h->next;
-	}while(h!=Head);
-	printf("\n");
+		last = last->next;
+	}while( last->next != Head );
+
+	// Create new node and append to list, then recirculate the list
+	struct Node *t = (struct Node*)malloc(sizeof(struct Node));
+	t->data = n;
+	t->next = Head;
+	last->next = t;
+
+	Display(Head);
 }
+
+void DisplayNode(struct Node *p)
+{
+	printf("Data,Next: %d,%d\n", p->data, p->next->data);
+}
+
+int Length(){
+
+	int length = 0;
+
+	struct Node *t = Head;
+	while( t->next != Head ) {
+		length++;
+		t = t->next;
+	} length++;
+	return length;
+}
+
+void Insert(int pos, int n)
+{
+	if( pos < 0 || pos > Length() )
+		return;
+
+	printf("Inserting node %d at position %d\n", n, pos);
+	struct Node *t;
+
+	if(pos == 0){	
+		struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+		p->data = n;
+
+		if(Head == NULL){
+			Head = p;
+			p->next = Head;
+		} else {
+			t = Head;
+			while( t->next != Head) t = t->next;
+			t->next = p;
+			p->next = Head;
+			Head = p;
+		}
+	} else { 
+		t = Head;
+
+		for(int i = 0; i < pos-1; i++){ 
+			t = t->next; 
+		}
+
+		struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+		p->data = n;
+		p->next = t->next;
+		t->next = p;
+	}
+	Display(Head);
+}
+	
+void Delete(int pos){
+
+	if( pos < 0 || pos > Length())
+		return;
+
+	printf("Deleting node %d from list\n", pos);
+	struct Node *t = Head;
+	struct Node *last = Head;
+
+	if(pos == 0){
+		while (t->next != Head){
+			last = t;
+			t = t->next;
+		}
+		Head = Head->next;
+		free(t->next);
+		t->next = Head;
+	} else {
+		int i = 0;
+		do{
+			last = t;
+			t = t->next;
+			i++;
+		} while( i < pos - 1);
+
+		last->next = t->next;
+		free(t);
+	}
+	Display(Head);
+}
+
 
 int main()
 {
-	int A[] = {2, 3, 4, 5, 6};
-	create(A,5);
-	Display(Head);
+	int A[] = {1, 2, 3};
+	createCirc(A, 3);
+	
+	Append(4);
+	Insert(4, 5);
+	Insert(0, 0);
+	Delete(0);
+	Delete(3);
+
 
 	return 0;
 }
+
